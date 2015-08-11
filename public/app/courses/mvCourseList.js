34a8f9d2e86e2a45470575ecd2a1fae76feb68d9
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2015-08-10 22:54:33 -0400

    - Adding sortBy and Filtering features to coure-list
     - Fixing up UI to look nicer in the search form aream

--- public/app/courses/mvCourseList.js
+++ public/app/courses/mvCourseList.js
@@ -2,5 +2,18 @@ angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse) 
 
     // Queries the mvCourse resource from the mvCourse service to get list of courses 
     $scope.courses = mvCourse.query(); 
+
+
+    // Creates options for select box to sort list by  
+    // Based of the properties of the courses object 
+    $scope.sortOptions = [
+
+        {value: "name", text: "Sort by title"},
+        {value: "published", text: "Sort by published date"}
+    ];
+
+
+    // Sets an initial sort order by default to be by title
+    $scope.sortOrder = $scope.sortOptions[0].value; 
     
 })
